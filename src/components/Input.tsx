b7442c2e5dab3173a0dbd0bb756f9aed1acc3350
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-07-23 23:13:56 +0200

    feat: Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,15 @@
+import { JSX } from "solid-js/jsx-runtime";
+
+interface Props extends JSX.HTMLAttributes<HTMLInputElement> {
+	type: string
+}
+
+export function Input({ type = "text", ...restProps }: Props) {
+  return (
+    <input
+      type={type}
+      class="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-slate-300 rounded accent-blue"
+      {...restProps}
+    />
+  );
+}
