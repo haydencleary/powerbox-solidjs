e2b142af192ffd39d2f109d14da7b0cbe495851d
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-07-25 22:56:17 +0200

    fix: Input props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,19 @@
+import {splitProps} from "solid-js"
 import { JSX } from "solid-js/jsx-runtime";
 
 interface Props extends JSX.HTMLAttributes<HTMLInputElement> {
   type: string;
 }
 
-export function Input({ type = "text", ...restProps }: Props) {
+export function Input(props: Props) {
+  const [local, inputProps] = splitProps(props, ['type']);
+  const type = () => local.type || "text";
+
   return (
     <input
-      type={type}
+      type={type()}
       class="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-slate-300 rounded accent-blue"
-      {...restProps}
+      {...inputProps}
     />
   );
 }
