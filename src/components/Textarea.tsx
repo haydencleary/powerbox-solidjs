54c4bc943e0b22621c1803c36f32b1ec90849f50
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-07-25 23:00:01 +0200

    fix: Textarea props

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,16 +1,11 @@
-export function Textarea({
-  name,
-  id,
-  placeholder = "",
-  value,
-}: HTMLTextAreaElement) {
+export function Textarea(props: HTMLTextAreaElement) {
   return (
     <textarea
       rows="4"
-      name={name}
-      id={id}
-      placeholder={placeholder}
-      value={value}
+      name={props.name}
+      id={props.id}
+      placeholder={props.placeholder || ""}
+      value={props.value}
       class="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-slate-300 rounded"
     />
   );
